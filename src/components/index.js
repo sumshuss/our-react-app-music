58805cc729288872f7c4c849207d59249f54b3d0
Dakshitha Mak <dakshitha1992@hotmail.com>
2022-07-05 12:30:02 +0100

    added some structure and tried to add like button

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Header} from './Header'
+export {default as JustinBieber} from './JustinBieber'
+export {default as KateBush} from './KateBush'
+export {default as Rammstein} from './Rammstein'
